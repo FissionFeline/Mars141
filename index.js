af7540b5d1ef4878163e082ee4b00a1d19d30026
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2021-09-17 20:21:35 +0200

    Push

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 // Require the necessary discord.js classes
 const Discord = require('discord.js');
 const { token } = require('./credentials.json');
-const newUsers = [];
 // Create a new client instance
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MEMBERS"] })
-
-const Addrole = (member) => {
-  var role= member.guild.roles.cache.find(role => role.id === "883020079086854165");
-  member.roles.add(role);
-}
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MEMBERS","DIRECT_MESSAGES","GUILD_MESSAGES"] })
 
 client.once('ready', () => {
-  client.user.setStatus("Supporting the allies")
 	console.log('Ready!');
+  client.user.setActivity("supporting the battle"); 
 });
 
 client.on('guildMemberAdd', member => {
   const channel = client.channels.cache.find(channel => channel.name === "general")
-  channel.send("Welcome to the server "+member.displayName)
-  Addrole(member);
+  channel.send("Welcome to the server "+member.displayName + "\nCome and greet us and when youre ready join our subreddit \nhttps://www.reddit.com/r/anti_zoo_crusaders/")
+  const myGuild = client.guilds.cache.get('881734698399846411');
+  const myRole = myGuild.roles.cache.find(role => role.name === 'crusader');
+  member.roles.add(myRole);
 });
 
 client.login(token);
